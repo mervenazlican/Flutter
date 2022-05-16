1d8a0eab2389e5340ff6c3f85093845654cf11c9
Merve Nazlıcan <merve.nazlican.1@gmail.com>
2022-05-16 22:23:44 +0300

    Ödev 1

---
+++ lib/giris/odev1.dart
@@ -0,0 +1,46 @@
+void main(){
+  int islemNo = 10;
+  int ogrNo = 140255020;
+  String ogrAd = "Merve";
+  String ogrSoyad = "Nazlıcan";
+  int sinif = 3;
+  bool cinsiyet =true;
+  String dTarih = "1996";
+  int kitapNo = 52102;
+  String kitapAdi = "Seyir";
+  String aTarih = "Mayıs";
+  String vTarih = "Ağustos";
+  int sayfaSayisi = 348;
+  int isbnNo = 203654;
+  int yazarNo = 165327;
+  String yazarAd = "Piraye";
+  String yazarSoyad = "Özgün";
+  int turNo = 2;
+  String turAdi = "Kurgu";
+  int puan = 10;
+
+
+
+  print("İşlem numarası : $islemNo");
+  print("Öğrenci Numarası : $ogrNo");
+  print("Öğrenci Adı : $ogrAd");
+  print("Öğrenci Soyadı : $ogrSoyad");
+  print("Sınıfı : $sinif");
+  print("Cinsiyet : $cinsiyet");
+  print("Doğum Tarihi : $dTarih");
+  print("Kitap Numarası : $kitapNo");
+  print("Kitap Adı : $kitapAdi");
+  print("Alınan Tarih : $aTarih");
+  print("Verilen Tarih : $vTarih");
+  print("Sayfa Sayısı : $sayfaSayisi");
+  print("İsbn Numarası : $isbnNo");
+  print("Yazar Numarası : $yazarNo");
+  print("Yazar Adı : $yazarAd");
+  print("Yazar Soyadı : $yazarSoyad");
+  print("Tür Numarası : $turNo");
+  print("Tür Adı : $turAdi");
+  print("Puan : $puan");
+
+}
+
+
